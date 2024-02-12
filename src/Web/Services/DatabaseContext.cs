a0cc8f60a2ea3a7ac3a33b907329e1447dc93d2c
Chris Usher <chrisushertester@gmail.com>
2024-02-12 22:25:58 +0000

    Add ConnectionString to Configuration

--- src/Web/Services/DatabaseContext.cs
+++ src/Web/Services/DatabaseContext.cs
@@ -1,10 +1,18 @@
+using System.Configuration;
 using System.Data.Entity;
+using System.Data.SqlClient;
 using SimpleFrameworkApp.Services.Tables;
 
 namespace SimpleFrameworkApp.Services
 {
     public class DatabaseContext : DbContext
     {
+        public DatabaseContext()
+        { 
+            var connectionString = ConfigurationManager.ConnectionStrings["Framework"].ConnectionString;
+            Database.Connection.ConnectionString = connectionString;
+        }
+        
         public DbSet<VersionHistory> VersionHistory { get; set; }
     }
 }
